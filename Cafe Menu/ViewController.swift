dd0e25c38e758ee1573c4f5e7590519c6ff3377a
Chris Carty <chriscarty2@gmail.com>
2018-07-12 22:14:22 -0400

    Initial Commit

---
+++ Cafe Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cafe Menu
+//
+//  Created by Chris Carty on 7/12/18.
+//  Copyright © 2018 Chris Carty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
